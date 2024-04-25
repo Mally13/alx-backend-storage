fbe0fb3950b617197c8ed753f79222755cbeab28
Mally13 <mallymutuku13@gmail.com>
2024-04-25 19:57:45 +0300

    Task 1: ENUM and DEFAULT

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- Creates a table users following these requirements:
+--  id, integer, never null, auto increment and primary key
+--  email, string (255 characters), never null and unique
+--  name, string (255 characters)
+--  country, enumeration of countries: US, CO and TN,
+--  never null (= default will be the first element of the enumeration, here US)
+
+CREATE TABLE IF NOT EXISTS users (
+    id  INTEGER PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
