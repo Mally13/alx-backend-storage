26e2eedc535083f6385f75a079261fa2f22a3a9e
Mally13 <mallymutuku13@gmail.com>
2024-04-30 18:28:14 +0300

    Task 7: delete by match

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,9 +7,6 @@ List all documents in a collection
 def list_all(mongo_collection):
     """
     lists all documents in a collection
-
-    :param mongo_collection:
-    :return:
     """
     documents = mongo_collection.find()
     return documents
